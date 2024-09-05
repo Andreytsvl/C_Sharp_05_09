19ffbf86e585a9f1409787c9df028c3b1a178cac
unknown <andreytsvl15@gmail.com>
2024-09-05 08:38:00 +0300

    lect2

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+int[] arr = new int [5];
+arr[0]=5;
+arr[1]=8;
+arr[2]=40;
+arr[3]=2;
+arr[4]=5;
+Console.WriteLine(arr[0]);
+Console.WriteLine(arr[2]);
+Console.WriteLine(arr[3]);
+int[] arr1 = {1,2,3,4,5};
+Console.WriteLine(arr1[0]);
+Console.WriteLine(arr1[2]);
+Console.WriteLine(arr1[4]);
